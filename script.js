31ef580cc60efb2b746aa6aac27e916537fae4d1
anitazgb <anitazgb1@gmail.com>
2023-04-30 16:25:20 -0700

    https://github.com/anitazgb/P209HW4.git

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+// define Food constructor
+function Food(pName, pCalories) {
+    this.name = pName;
+    this.calories = parseInt(pCalories);
+  }
 
+  // initialize food array with two items
+  var foods = [
+    new Food("Banana", 100),
+    new Food("Hot Dog", 470)
+  ];
+
+  // display food array in console
+  console.log(foods);
+
+ 
